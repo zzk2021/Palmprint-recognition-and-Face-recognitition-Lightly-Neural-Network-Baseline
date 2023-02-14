47578c5351eca1e74cbf9d1fc4b5a88035879881
zzk@2020 <73774678+zzk2021@users.noreply.github.com>
2023-02-14 23:53:40 +0800

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,5 +1,12 @@
-import torch
+import os
+import time
+
 import numpy as np
+import torch
+from sklearn.metrics import precision_score, f1_score, recall_score, confusion_matrix, accuracy_score, \
+    ConfusionMatrixDisplay
+from torch.cuda.amp import autocast
+import matplotlib.pyplot as plt
 
 def euclidean_distance(qf, gf):
     m = qf.shape[0]
@@ -9,6 +16,93 @@ def euclidean_distance(qf, gf):
     dist_mat.addmm_(1, -2, qf, gf.t())
     return dist_mat.cpu().numpy()
 
+def correct_count(output, target, topk=(1,)):
+    """Computes the top k corrrect count for the specified values of k"""
+    maxk = max(topk)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)
+        res.append(correct_k)
+    return res
+
+@autocast()
+def evaluate(logger, net, dataloader, loss_fn, Ncrop=True, logname="model", device="cuda:0"):
+    net = net.eval()
+    loss_tr, n_samples = 0.0, 0.0
+
+    y_pred = []
+    y_gt = []
+
+    correct_count1 = 0
+    correct_count2 = 0
+    epoch_time = 0
+    with torch.no_grad():
+        for data in dataloader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            if Ncrop:
+                # fuse crops and batchsize
+                bs, ncrops, c, h, w = inputs.shape
+                inputs = inputs.view(-1, c, h, w)
+
+                # forward
+                start = time.time()
+                outputs = net(inputs)
+                end = time.time()
+                epoch_time += ( end - start)
+                # combine results across the crops
+                outputs = outputs.view(bs, ncrops, -1)
+                outputs = torch.sum(outputs, dim=1) / ncrops
+            else:
+                start = time.time()
+                outputs = net(inputs)
+                end = time.time()
+                epoch_time += ( end - start)
+
+            #loss = loss_fn(outputs, labels)
+
+            # calculate performance metrics
+            #loss_tr += loss.item()
+
+            # accuracy
+            counts = correct_count(outputs, labels, topk=(1, 2))
+            correct_count1 += counts[0].item()
+            correct_count2 += counts[1].item()
+
+            _, preds = torch.max(outputs.data, 1)
+            n_samples += labels.size(0)
+
+            y_pred.extend(pred.item() for pred in preds)
+            y_gt.extend(y.item() for y in labels)
+
+    acc1 = 100 * correct_count1 / n_samples
+    acc2 = 100 * correct_count2 / n_samples
+    epoch_time = epoch_time / n_samples
+    fps = 1 / epoch_time
+    #loss = loss_tr / n_samples
+    logger.info("----------------validation--------------------")
+    logger.info("validation. Avg time per image: %2.6f" % epoch_time)
+    logger.info("validation. Avg FPS: %2.6f" % fps)
+    logger.info("validation. Top 1 Accuracy: %2.6f %%" % acc1)
+    logger.info("validation. Top 2 Accuracy: %2.6f %%" % acc2)
+   # logger.info("validation. Loss: %2.6f" % loss)
+    logger.info("validation. Precision: %2.6f" % precision_score(y_gt, y_pred, average='micro'))
+    logger.info("validation. Recall: %2.6f" % recall_score(y_gt, y_pred, average='micro'))
+    logger.info("validation. F1 Score: %2.6f" % f1_score(y_gt, y_pred, average='micro'))
+    logger.info("validation. Acc: %2.6f"%accuracy_score(y_gt, y_pred))
+    cm = confusion_matrix(y_true=y_gt, y_pred=y_pred, normalize='true')
+    # 画出混淆矩阵
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=[1,2,3,4,5,6,7])
+    disp.plot()
+    if not os.path.exists("./log/%s/"%logname):
+        os.mkdir("./log/%s"%logname)
+    plt.savefig('./log/%s/ConfusionMatrix.png'%logname, bbox_inches='tight')
 
 def cosine_similarity(qf, gf):
     epsilon = 0.00001
