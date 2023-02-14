97afa3f6b7b37647ebae2066e5c1f764c68ce4a6
zzk@2020 <73774678+zzk2021@users.noreply.github.com>
2023-02-14 23:54:37 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,20 +6,22 @@ from datasets import make_dataloader
 from model import make_model
 from processor import do_inference
 from utils.logger import setup_logger
-
-
+from utils.metrics import evaluate
+import torch.nn as nn
 if __name__ == "__main__":
     cfg = Config()
     log_dir = cfg.LOG_DIR
-    logger = setup_logger('{}.test'.format(cfg.PROJECT_NAME), log_dir)
+    cfg.LOG_NAME = cfg.LOG_NAME + "_test"
+    cfg.PRETRAIN_CHOICE = "no"
+    logger = setup_logger('{}.test'.format(cfg.PROJECT_NAME), log_dir,cfg)
     os.environ['CUDA_VISIBLE_DEVICES'] = cfg.DEVICE_ID
     cudnn.benchmark = True
 
-    train_loader, val_loader, num_query, num_classes = make_dataloader(cfg)
+    train_loader, val_loader, test_loader, num_query, num_classes = make_dataloader(cfg)
+    loss_fn = nn.CrossEntropyLoss()
     model = make_model(cfg, num_classes)
     model.load_param(cfg.TEST_WEIGHT)
-
-    do_inference(cfg,
-                 model,
-                 val_loader,
-                 num_query)
+    logger.info("-----------val dataloader set-----------")
+    evaluate(logger,model, val_loader, loss_fn, cfg.NCROP,cfg.LOG_NAME)
+    logger.info("-----------test dataloader set-----------")
+    evaluate(logger,model, test_loader, loss_fn,cfg.NCROP,cfg.LOG_NAME)
